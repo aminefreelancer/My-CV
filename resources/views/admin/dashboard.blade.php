fdc79f5d4949a5fe6ec1bd82dde7b9cd7c7c3df0
m.akacha <akachamohamedamine@gmail.com>
2022-01-03 13:03:32 +0100

    Config Login & Dashboard theme

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,31 @@
+@extends('admin.layouts.app')
+
+@section('content')
+    <!--Page header-->
+    <div class="page-header">
+        <div class="page-leftheader">
+            <h4 class="page-title mb-0 text-primary">Dashboard</h4>
+        </div>
+        <div class="page-rightheader">
+            <div class="btn-list">
+                <button class="btn btn-outline-primary"><i class="fe fe-download me-2"></i>
+                    Import
+                </button>
+                
+            </div>
+        </div>
+    </div>
+    <!--End Page header-->
+
+    <!-- Row -->
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-body">
+                    Something text type here.....
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End Row -->
+@endsection
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,122 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+	<head>
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+		<!-- Meta data -->
+		<meta charset="UTF-8">
+		<meta name='viewport' content='width=device-width, initial-scale=1.0, user-scalable=0'>
+		<meta content="" name="description">
+		<meta content="" name="author">
+		<meta name="keywords" content="">
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+		<!-- Title -->
+		<title>Mohamed Amine AKACHA - Full Stack Developer </title>
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+		<!--Favicon -->
+		<link rel="icon" href="assets/images/brand/favicon.ico" type="image/x-icon"/>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+		<!--Bootstrap css -->
+		<link href="assets/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
+		<!-- Style css -->
+		<link href="assets/css/style.css" rel="stylesheet" />
+		<link href="assets/css/dark.css" rel="stylesheet" />
+		<link href="assets/css/skin-modes.css" rel="stylesheet" />
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+		<!-- Animate css -->
+		<link href="assets/css/animated.css" rel="stylesheet" />
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+		<!---Icons css-->
+		<link href="assets/css/icons.css" rel="stylesheet" />
 
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
+	    <!-- Color Skin css -->
+		<link id="theme" href="assets/colors/color1.css" rel="stylesheet" type="text/css"/>
 
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+	</head>
 
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
+	<body class="register-2">
+		<div class="page">
+			<div class="page-content">
+				<div class="container">
+					<div class="row">
+						<div class="col mx-auto">
+							<div class="row justify-content-center">
+								<div class="col-md-4">
+									
+									<div class="card">
+										<div class="card-body">
+											<div class="text-center mb-3">
+												<h1 class="mb-2">Log In</h1>
+												<a href="javascript:void(0);" class="">Welcome Back !</a>
+											</div>
+											<form class="mt-5" method="POST" action="{{ route('login') }}">
+                                                @csrf
+												<div class="input-group mb-4">
+														<div class="input-group-text">
+															<i class="fe fe-user"></i>
+														</div>
+													<input type="text" class="form-control @error('email') is-invalid @enderror" placeholder="Email" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                                    @error('email')
+                                                        <span class="invalid-feedback" role="alert">
+                                                            <strong>{{ $message }}</strong>
+                                                        </span>
+                                                    @enderror
+												</div>
+												<div class="input-group mb-4">
+													<div class="input-group" id="Password-toggle1">
+														<a href="" class="input-group-text">
+														  <i class="fe fe-eye" aria-hidden="true"></i>
+														</a>
+														<input class="form-control @error('password') is-invalid @enderror" type="password" placeholder="Password" name="password" required autocomplete="current-password">
+                                                        @error('password')
+                                                            <span class="invalid-feedback" role="alert">
+                                                                <strong>{{ $message }}</strong>
+                                                            </span>
+                                                        @enderror
+													</div>
+												</div>
+												<div class="form-group">
+													<label class="custom-control custom-checkbox">
+														<input type="checkbox" class="custom-control-input" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}/>
+														<span class="custom-control-label" for="remember">Remember me</span>
+													</label>
+												</div>
+												<div class="form-group text-center mb-3">
+													<button class="btn btn-primary btn-lg w-100 br-7">Log In</button>
+												</div>
+											</form>
+										</div>
+									</div>
+                                    
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
+		<!-- Jquery js-->
+		<script src="assets/js/jquery.min.js"></script>
 
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-@endsection
+		<!-- Bootstrap5 js-->
+		<script src="assets/plugins/bootstrap/popper.min.js"></script>
+		<script src="assets/plugins/bootstrap/js/bootstrap.min.js"></script>
+
+		<!--Othercharts js-->
+		<script src="assets/plugins/othercharts/jquery.sparkline.min.js"></script>
+
+		<!-- Circle-progress js-->
+		<script src="assets/js/circle-progress.min.js"></script>
+
+		<!-- Jquery-rating js-->
+		<script src="assets/plugins/rating/jquery.rating-stars.js"></script>
+
+		<!-- Show Password -->
+		<script src="assets/js/bootstrap-show-password.min.js"></script>
+
+		<!-- Custom js-->
+		<script src="assets/js/custom.js"></script>
+
+	</body>
+</html>
